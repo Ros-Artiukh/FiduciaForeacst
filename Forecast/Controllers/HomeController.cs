52d19a43ca3c17e73e4c68beae1c4c6cae1da207
Rostislav Artiukh <r.artiukh97@gmail.com>
2021-02-24 16:17:40 +0200

    add name for input

---
+++ Forecast/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Forecast.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+namespace Forecast.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult City(string city)
+        {
+            var content = new Api().getForecast(city);
+            return View(content);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Forecast/Models/Api.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Forecast.Models
+{
+        public class Main
+        {
+            public string Temp_min {get;set;}
+            public string Temp_max { get; set; }
+        }
+
+        public class Weather
+        {
+            public string Main { get; set; }
+        }
+
+        public class SeparatorResponse
+        {
+            public List<Weather> Weather { get; set; }
+            public Main Main { get; set; }
+        }
+
+        public class Api
+        {
+            public async Task<SeparatorResponse> getForecast(string city) {
+                var client = new HttpClient();
+                var url = $"api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=c43b508482520a8beaf781d2f0ff750c";
+                var response = await client.GetAsync($"http://{url}");
+                string json = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<SeparatorResponse>(json);
+                return result;
+            }
+       
+        }
+}
